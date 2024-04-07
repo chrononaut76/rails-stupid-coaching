2142960dd9f2b920cdd9c5aa3067ff45f91b1cb6
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-04-06 22:49:42 -0500

    Updated contents of application_system_test_case.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,7 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu window-size=1400,900])
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  Capybara::Selenium::Driver.new(app, browser: :headless_chrome, options: options)
 end
 Capybara.save_path = Rails.root.join('tmp/capybara')
 Capybara.javascript_driver = :headless_chrome
